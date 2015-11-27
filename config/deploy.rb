4a390f7e9ad5d836d9363df5397c1c7f272047d7
sushant12 <sushatnbajracharya@gmail.com>
2015-11-27 20:58:41 +0545

    removed fonts file

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,7 +92,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "service #{user} restart"
-      invoke :'passenger:restart'
+
     end
   end
 end
